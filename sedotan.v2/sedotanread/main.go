37f35b1ef1ab8a73445c1078f0e8de82a6f33b99
kranjcarz <kranjcarz@yahoo.com>
2016-03-14 18:13:16 +0700

    add snapshot & logs

--- sedotan.v2/sedotanread/main.go
+++ sedotan.v2/sedotanread/main.go
@@ -12,12 +12,15 @@ import (
 	"time"
 	"github.com/eaciit/cast"
 	"strconv"
+	"io/ioutil"
 )
 
 var (
 	fLocation = flag.String("pathfile", "", "Full path file location include filename and pattern") //support using environment variable EC_DATA_PATH
-	fReadType = flag.String("readtype", "", "read type sedotan file")   
+	fReadType = flag.String("readtype", "", "read type sedotan file")
+	fNameid = flag.String("nameid", "", "read type sedotan file")   
 	tLocation string
+	tNameid string
 )
 
 type Grabber struct {
@@ -27,9 +30,6 @@ type Grabber struct {
 }
 
 func main() {
-	// run test
-	// go run main.go -readtype="history" -pathfile="E:\EACIIT\src\github.com\eaciit\sedotan\sedotan.v2\test\hist\HIST-GRABDCE-20160225.csv"
-
 	var err error
 	var datastring string
 	// var data toolkit.M
@@ -48,42 +48,104 @@ func main() {
 	flag.Parse()
 
 	tReadType := toolkit.ToString(*fReadType)
+	tLocation = toolkit.ToString(*fLocation)
+	tNameid = toolkit.ToString(*fNameid)
 	//snapshot,history,rechistory,logfile,[daemonlog]
 
 	//=========== Parse other flag ===========
 	// HERE
 	//========================================
 
-	switch tReadType {
-	case "snapshot":
-		//snapshot function include dataset
-	case "history":
+	// switch tReadType {
+	// case "snapshot":
+	// 	module := NewHistory(tLocation)
+	// 	datatemp, err = module.OpenHistory()
+	// 	datastring = toolkit.JsonString(datatemp)
+	// 	err = toolkit.UnjsonFromString(datastring, &dataset)
+	// case "history":
+	// 	// go run main.go -readtype="history" -pathfile="E:\EACIIT\src\github.com\eaciit\sedotan\sedotan.v2\test\hist\HIST-GRABDCE-20160225.csv"
+	// 	module := NewHistory(tLocation)
+	// 	datatemp, err = module.OpenHistory()
+	// 	datastring = toolkit.JsonString(datatemp)
+	// 	err = toolkit.UnjsonFromString(datastring, &dataset)
+	// 	fmt.Println(datastring)
+	// case "rechistory":
+	// 	module := NewHistory(tLocation)
+	// 	datatemp, err = module.OpenHistory()
+	// 	datastring = toolkit.JsonString(datatemp)
+	// 	err = toolkit.UnjsonFromString(datastring, &dataset)
+	// case "logfile":
+	// 	// go run main.go -readtype="logfile" -pathfile="E:\EACIIT\src\github.com\eaciit\sedotan\sedotan.v2\test\log\LOG-GRABDCE-20160225"
+	// 	test, err := GetLogs()
+	// 	if err != nil {
+			
+	// 	}
+	// 	datatemp = append(datatemp,test)
+	// 	datastring = toolkit.JsonString(datatemp)
+	// case "daemonlog":
+	// 	test, err := GetLogs()
+	// 	if err != nil {
+			
+	// 	}
+	// 	datatemp = append(datatemp,test)
+	// 	datastring = toolkit.JsonString(datatemp)
+	// default:
+	// 	err = errors.New(fmt.Sprintf("-readtype cannot empty or get wrong format"))
+	// }
+
+	if tReadType == "snapshot" {
+		module := GetDirSnapshot(tNameid)
+		SnapShot, err := module.OpenSnapShot(tNameid)
+		if err != nil {
+			fmt.Sprintf("ERROR: %s", err)
+		}
+		datastring = toolkit.JsonString(SnapShot)
+		err = toolkit.UnjsonFromString(datastring, &dataset)
+		container.Set("DATA", dataset)
+	}else if tReadType == "history" || tReadType == "rechistory"{
 		module := NewHistory(tLocation)
 		datatemp, err = module.OpenHistory()
+		if err != nil {
+			fmt.Sprintf("ERROR: %s", err)
+		}
 		datastring = toolkit.JsonString(datatemp)
 		err = toolkit.UnjsonFromString(datastring, &dataset)
-	case "rechistory":
-		//rechistory function include dataset
-	case "logfile":
-		//logfile function include dataset
-	case "daemonlog":
-		//daemonlog function include dataset
-	default:
+		container.Set("DATA", dataset)
+	}else if tReadType == "logfile" || tReadType == "daemonlog" {
+		logs, err := GetLogs()
+		if err != nil {
+			fmt.Sprintf("ERROR: %s", err)
+		}
+		datatemp = append(datatemp,logs)
+		datastring = toolkit.JsonString(datatemp)
+		container.Set("DATA", datastring)
+	}else{
+		container.Set("DATA", dataset)
 		err = errors.New(fmt.Sprintf("-readtype cannot empty or get wrong format"))
 	}
 
 	container.Set("ERROR", err)
-	container.Set("DATA", dataset)
-
 	outputstring := toolkit.JsonString(container)
-
+ 
 	fmt.Printf("%s", outputstring)
+}
+
+func GetLogs() (interface{}, error) {
+	filepath := toolkit.ToString(*fLocation)
+
+	bytes, err := ioutil.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+	logs := string(bytes)
+
+	return logs, err
 }
 
 func (w *Grabber) OpenHistory() ([]interface{}, error) {
 	var history = []interface{}{} //toolkit.M{}
 	var config = map[string]interface{}{"useheader": true, "delimiter": ",", "dateformat": "MM-dd-YYYY"}
-	tLocation = toolkit.ToString(*fLocation)
+
 	ci := &dbox.ConnectionInfo{tLocation, "", "", "", config}
 	c, err := dbox.NewConnection("csv", ci)
 	if err != nil {
@@ -112,6 +174,7 @@ func (w *Grabber) OpenHistory() ([]interface{}, error) {
 
 	for i, v := range ds {
 		castDate, _ := time.Parse(time.RFC3339, v.Get("grabdate").(string))
+
 		w.humanDate = cast.Date2String(castDate, "YYYY/MM/dd HH:mm:ss")
 		w.rowgrabbed, _ = strconv.ParseFloat(fmt.Sprintf("%v", v.Get("rowgrabbed")), 64)
 		w.rowsaved, _ = strconv.ParseFloat(fmt.Sprintf("%v", v.Get("rowgrabbed")), 64)
@@ -140,4 +203,56 @@ func NewHistory(nameid string) *Grabber {
 	w.filepathName = path
 	w.nameid = nameid
 	return w
+}
+
+func GetDirSnapshot(nameid string) *Grabber {
+	w := new(Grabber)
+	w.filepathName = tLocation
+	w.nameid = nameid
+	return w
+}
+
+func (w *Grabber) OpenSnapShot(Nameid string) ([]interface{}, error) {
+	var snapShot = []interface{}{} //toolkit.M{}
+	var config = map[string]interface{}{"useheader": true, "delimiter": ",", "dateformat": "MM-dd-YYYY"}
+	ci := &dbox.ConnectionInfo{w.filepathName, "", "", "", config}
+	c, err := dbox.NewConnection("csv", ci)
+	if err != nil {
+		return snapShot, err
+	}
+
+	err = c.Connect()
+	if err != nil {
+		return snapShot, err
+	}
+	defer c.Close()
+
+	csr, err := c.NewQuery().Select("*").Where(dbox.Eq("Id", Nameid)).Cursor(nil)
+	if err != nil {
+		return snapShot, err
+	}
+	if csr == nil {
+		return snapShot, errors.New("Cursor not initialized")
+	}
+	defer csr.Close()
+	ds := []toolkit.M{}
+	err = csr.Fetch(&ds, 0, false)
+	if err != nil {
+		return snapShot, err
+	}
+	for _, v := range ds {
+		var addToMap = toolkit.M{}
+		addToMap.Set("id", v.Get("Id"))
+		addToMap.Set("starttime", v.Get("Starttime"))
+		addToMap.Set("endtime", v.Get("Endtime"))
+		addToMap.Set("grabcount", v.Get("Grabcount"))
+		addToMap.Set("rowgrabbed", v.Get("Rowgrabbed"))
+		addToMap.Set("errorfound", v.Get("Errorfound"))
+		addToMap.Set("lastgrabstatus", v.Get("Lastgrabstatus"))
+		addToMap.Set("grabstatus", v.Get("Grabstatus"))
+		addToMap.Set("note", v.Get("Note"))
+
+		snapShot = append(snapShot, addToMap)
+	}
+	return snapShot, nil
 }
